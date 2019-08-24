4b082f5f7627f3866a55bcfb7e0559794fc91ee8
Mike Barkmin <mike.barkmin@uni-due.de>
2019-08-24 18:21:11 +0200

    Add sentry

--- server/main.py
+++ server/main.py
@@ -1,4 +1,11 @@
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
 from lesbar import create_app
+
+sentry_sdk.init(
+    dsn="https://b2d8172ecb894f61a9a322788591c172@sentry.io/1540715",
+    integrations=[FlaskIntegration()],
+)
 
 app = create_app()
 
