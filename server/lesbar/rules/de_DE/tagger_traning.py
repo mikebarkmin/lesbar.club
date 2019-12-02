38727d3f4194bbec5482c7659adcf5a641d88ceb
Mike Barkmin <mike.barkmin@uni-due.de>
2019-12-02 22:06:02 +0100

    New better model

--- server/lesbar/rules/de_DE/tagger_traning.py
+++ server/lesbar/rules/de_DE/tagger_traning.py
@@ -2,10 +2,10 @@ import nltk
 import random
 import re
 import pickle
-from nltk.tag.sequential import ClassifierBasedTagger
-from lesbar.rules.de_DE.ClassifierBasedGermanTagger import ClassifierBasedGermanTagger
+from ClassifierBasedGermanTagger import ClassifierBasedGermanTagger
 
 
+print("Read: Part A")
 corpA = nltk.corpus.reader.ConllCorpusReader(
     ".",
     "part_A.conll",
@@ -14,6 +14,7 @@ corpA = nltk.corpus.reader.ConllCorpusReader(
     separator="\t",
 )
 
+print("Read: Part B")
 corpB = nltk.corpus.reader.ConllCorpusReader(
     ".",
     "part_B.conll",
@@ -22,12 +23,22 @@ corpB = nltk.corpus.reader.ConllCorpusReader(
     separator="\t",
 )
 
-train_sents = list(corpA.tagged_sents())
-test_sents = list(corpB.tagged_sents())
+print("Combining")
+tagged_sents = list(corpA.tagged_sents()) + list(corpB.tagged_sents())
+random.shuffle(tagged_sents)
 
+split_perc = 0.1
+split_size = int(len(tagged_sents) * split_perc)
+
+print("Splitting")
+train_sents, test_sents = tagged_sents[split_size:], tagged_sents[:split_size]
+
+print("Training")
 tagger = ClassifierBasedGermanTagger(train=train_sents)
+print("Evaluating")
 accuracy = tagger.evaluate(test_sents)
-print(accuracy)
+
+print(f"Accuracy: {accuracy}")
 
 with open("nltk_german_pos.pickle", "wb") as f:
     pickle.dump(tagger, f)
