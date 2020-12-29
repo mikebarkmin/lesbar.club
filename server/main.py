d243b8962b214703eb6f1cec1ccd530246d99f04
Mike Barkmin <mike@barkmin.eu>
2020-12-29 20:09:04 +0100

    update image to fit gcloud run

--- server/main.py
+++ server/main.py
@@ -8,9 +8,11 @@ from nltk.tag.sequential import ClassifierBasedTagger
 if os.getenv("FLASK_ENV") == "producation" and os.getenv("SENTRY_DSN"):
     sentry_sdk.init(dsn=os.getenv("SENTRY_DSN"), integrations=[FlaskIntegration()])
 
+port = os.getenv("PORT", 8000)
+
 app = create_app()
 
 if __name__ == "__main__":
 
     # Only for debugging while developing
-    app.run(host="0.0.0.0", debug=True, port=8000)
+    app.run(host="0.0.0.0", debug=True, port=port)
