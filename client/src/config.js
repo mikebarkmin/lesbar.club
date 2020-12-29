4aeb3fe530ddf1c1e60c42ce0cd9ce14048c9c6a
Mike Barkmin <mike@barkmin.eu>
2020-12-29 20:08:44 +0100

    configure api url with env

--- client/src/config.js
+++ client/src/config.js
@@ -1,9 +1,9 @@
-let apiURL = 'https://api.lesbar.club';
+let apiURL = process.env.API_URL || "https://api.lesbar.club";
 
-if (process.env.NODE_ENV === 'development') {
-  apiURL = 'http://localhost:8000';
+if (process.env.NODE_ENV === "development") {
+  apiURL = "http://localhost:8000";
 }
 
 export default {
-  apiURL
+  apiURL,
 };
